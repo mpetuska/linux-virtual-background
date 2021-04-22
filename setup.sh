b2f4af7ce3b8f63ef7b5408a0a1cc3978d165295
Martynas Petuška <martynas.petuska@outlook.com>
2021-04-22 01:03:36 +0100

    INITIAL

--- setup.sh
+++ setup.sh
@@ -4,9 +4,13 @@ sudo groupadd video
 sudo usermod -a -G video $USER
 
 # https://elder.dev/posts/open-source-virtual-background/
-sudo apt install v4l2loopback-dkms v4l2loopback-utils
+sudo apt-get install v4l2loopback-dkms
+echo options v4l2loopback devices=1 video_nr=20 \
+  card_label="fakecam" exclusive_caps=1 | sudo tee \
+  /etc/modprobe.d/fakecam.conf
+echo v4l2loopback | sudo tee /etc/modules-load.d/fakecam.conf
 sudo modprobe -r v4l2loopback
-sudo modprobe v4l2loopback devices=1 video_nr=20 card_label="v4l2loopback" exclusive_caps=1
+sudo modprobe v4l2loopback
 
 # Install docker
 curl -fsSL https://get.docker.com -o get-docker.sh
